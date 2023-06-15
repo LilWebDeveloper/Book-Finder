472a01c8ff9deb9653899fbeea41b5a371016831
Kacper Falecki <kfalecki17@gmail.com>
2023-06-15 17:50:27 +0200

    delete: Delete useless import

--- src/tests/ExtendedRowTable.test.tsx
+++ src/tests/ExtendedRowTable.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import ExtendedRow from "../components/BooksList/ExtendedRow";
 import createData from "../utils/CreateData";
-import { Table, TableBody } from "@mui/material";
+import { Table } from "@mui/material";
 
 test("ExtendedRow renders the data correctly", () => {
   const row = createData(
